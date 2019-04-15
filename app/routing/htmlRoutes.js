cb2e512392468eb767a88e2478481f4f0b609574
Mike <michael.barbone@gmail.com>
2019-04-14 22:43:51 -0400

    Fix htmlRoutes.js home path typo

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,15 +4,15 @@ var path = require('path');
 // GET paths to handle routing when user visits page
 module.exports = function (app){
     app.get('/', function(req,res){
-        res.sendFile(path.join('/app/public/home.html'));
+        res.sendFile(path.join(__dirname,'../public/home.html'));
     });
 
     app.get('/survey', function(req,res){
-        res.sendFile(path.join(__dirname,'/app/public/survey.html'));
+        res.sendFile(path.join(__dirname,'../public/survey.html'));
     });
 
     // if no matching route, send user back home
     app.get('*', function(req,res){
-        res.sendFile(path.join('/app/public/home.html'));
+        res.sendFile(path.join('../public/home.html'));
     });
 };
